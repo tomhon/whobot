70c378b5ae87ee5fb8c4d3bc43623b0d394b6e4d
tomhon <tomhon@hotmail.com>
2016-05-04 14:46:30 -0700

    pointed to port 80

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ var client = amazon.createClient({
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || 80, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
