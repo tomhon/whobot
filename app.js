1da65c9993c9047306a323e01953cd09f4d5c6b2
tomhon <tomhon@hotmail.com>
2016-05-03 17:09:53 -0700

    added LUIS app

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
-    console.log('Welcome message sent');
-    session.send('Hello KIK Bot World');
+
+
+var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=8974d6fa-1500-46bc-9ac0-f5a1a1c30217&subscription-key=929a376180624437bc881e4501940e3e');
+bot.add('/', dialog);
+
+dialog.on('Search', function (session, args) {
+console.log(args.entities);
+    session.send('Bot version 1.2');
 });
 
 // Setup Restify Server
