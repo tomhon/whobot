908261ff0618fa1f7eda36221cb0e5c26572c348
Tom Honeybone <tomhon@microsoft.com>
2016-05-18 15:09:50 -0700

    adding session send to query function

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ var connection = new Connection(config);
               }
             });
             console.log(result);
+            session.send(result);
             result ="";
         });
 
